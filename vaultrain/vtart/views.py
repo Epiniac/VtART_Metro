730c20a0b6a451099f3d17ef9e2d666e6a378eef
Epiniac <jacqueschen92@gmail.com>
2024-04-01 14:20:36 +0200

    5 object logo done

--- vaultrain/vtart/views.py
+++ vaultrain/vtart/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+
 from vtart.models import VaultTrain
 
 # Create your views here.
 def index (request) :
+    mytrain = VaultTrain.objects.all()[:5]  # 5 first train, available
+
     lines = VaultTrain.objects.all()
-    return render(request , "index.html",{})
+    return render(request , "index.html",{
+        "name" : mytrain.name,
+        "destination" : mytrain.destination,
+        "logo" : mytrain.logo,
+        "time" : mytrain.time
+    })
 
 def detail (request):
     return render(request, "detail.html", {})
